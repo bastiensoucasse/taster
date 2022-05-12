e3cb1dc94355d8642b40d4354f3001ea19e5766d
Bastien Soucasse <bastien.soucasse@icloud.com>
2022-05-12 09:42:16 +0200

    Initial Commit

---
+++ Taster/TasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasterApp.swift
+//  Taster
+//
+//  Created by Bastien Soucasse on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
